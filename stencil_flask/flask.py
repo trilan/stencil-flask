055fea2008ba14c6f2cb0ab6c0ed9b4774f86a9e
Dima Kukushkin <dima@kukushkin.me>
2012-08-28 17:47:40 +0400

    Remove commented code.

--- stencil_flask/flask.py
+++ stencil_flask/flask.py
@@ -15,18 +15,3 @@ class Flask(Stencil):
     def generate_secret_key(self):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         return ''.join(choice(chars) for i in range(50))
-
-    """
-    def collect_resources(self):
-        super(Flask, self).collect_resources()
-        app_name = self.context['app_name']
-        for name in self.resources.keys():
-            if not name.startswith('project/'):
-                continue
-            resource = self.resources.pop(name)
-            name = '{0}{1}'.format(app_name, name[7:])
-            print name
-            print '{}->{}'.format(resource, resource.target)
-            print '\n'
-            self.resources[name] = resource
-    """
